0c5e61f7c9c06383b674213e58baa6dce79461dd
reinhardjakesacay <reinhardjakesacayforwork@gmail.com>
2024-03-05 22:15:46 +0800

    about us

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>About</title>
+</head>
+<body>
+
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+            <a href="#" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                <img src="images/logo.png" alt="Bootstrap" width="65" height="65">
+            </a>
+
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-light">Home</a></li>
+                <li><a href="#" class="nav-link px-2 link-light">About</a></li>
+                <li><a href="#" class="nav-link px-2 link-light">Menu</a></li>
+                <li><a href="#" class="nav-link px-2 link-light">Apps</a></li>
+                <li><a href="#" class="nav-link px-2 link-light">Contact</a></li>
+            </ul>
+
+            <div class="col-md-3 text-end">
+                <button type="button" class="btn me-2 btn-custom">Login</button>
+                <button type="button" class="btn btn-custom">Sign-up</button>
+            </div>
+        </header>
+    </div>
+
+
+
+
+    <div class="px-3 py-4 my-4 text-center">
+        <img class="d-block mx-auto mb-4" src="images/logo.png" alt="" width="100" height="100">
+    <h1 class="display-5 fw-bold text-white">Noirs Coffee</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4 px-3 text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi aliquam porro voluptas quasi molestias nesciunt magnam ipsum quae omnis minima similique culpa, sed accusantium quas illo quo, facere in maiores? In minus quo maiores. Ducimus id vero a error nemo?</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3 btn-custom">Primary button</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4 btn-custom">Secondary</button>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
--- register.php
+++ register.php
@@ -34,7 +34,23 @@
 
 
     <div class="container d-flex justify-content-center">
-        <form class="mt-5">
+        <form class="mt-3">
+            <div class="mb-3">
+                <label for="fullNameInputExample" class="form-label text-white">Full Name</label>
+                <input type="text" class="form-control" id="fullNameInputExample" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="ageInputExample" class="form-label text-white">Age</label>
+                <input type="text" class="form-control" id="ageInputExample" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="addressInputExample" class="form-label text-white">Address</label>
+                <input type="text" class="form-control" id="addressInputExample" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="phoneNumberInputExample" class="form-label text-white">Phone Number</label>
+                <input type="text" class="form-control" id="phoneNumberInputExample" aria-describedby="emailHelp">
+            </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label text-white">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -44,11 +60,10 @@
                 <label for="exampleInputPassword1" class="form-label text-white">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label text-white" for="exampleCheck1">Check me out</label>
+            <div class="mb-3">
+                <label class="form-check-label text-white" for="exampleCheck1">Already have an account? <a href="login.php">Click Here</a></label>
             </div>
-                <button type="submit" class="btn btn-primary btn-custom">Log In</button>
+                <button type="submit" class="btn btn-primary btn-custom">Register</button>
         </form>
     </div>
 
